31f666738117147fe5b1fdab0252c0a8542a27fe
Derick Bitencourte da Silva <derickbit7@gmail.com>
2025-04-06 21:54:09 -0300

    ex2 animação - skeleton

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -1,8 +1,20 @@
 import { Link } from "react-router-dom";
 import blackjack2 from "../../assets/blackjack2.jpg";
 import styles from "./Home.module.css";
+import { useState, useEffect } from "react";
 
 function Home() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simula o carregamento da imagem
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000); // 1.5 segundos
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <section className={styles.home_container}>
       <h1>Bem vindo(a) </h1>
@@ -10,7 +22,11 @@ function Home() {
         Apresentamos
         <p>
           <Link to="/partida" className={styles.playgame}>
-            <img src={blackjack2} alt="Imagem do jogo Blackjack 2.0" />
+            {loading ? (
+              <div className={styles.skeleton}></div>
+            ) : (
+              <img src={blackjack2} alt="Imagem do jogo Blackjack 2.0" />
+            )}
           </Link>
         </p>
         Clique na imagem para jogar
